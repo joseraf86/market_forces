5ab8b29a8c6c3af10b3e1ccf3d83e03d2c03ed75
Jose Guevara <joserafaelguevara@gmail.com>
2015-12-23 11:33:45 +0100

    Facility methods refactored and documented

--- app/facility.rb
+++ app/facility.rb
@@ -8,42 +8,46 @@ class Facility
 
   def initialize(**res_types)
     @level = 1
-    @res_maint = res_types[:maint]
-    @res_energy = res_types[:energy]
-    @res_output = res_types[:output]
-    @res_reserve = 0
+    @res_maint_type = res_types[:maint]
+    @res_energy_type = res_types[:energy]
+    @res_output_type = res_types[:output]
+    @res_reserve = Resource.new(@res_maint_type, 0)
     set_production_base_rate
     set_production_current_level_rate
   end
 
-  ##  EXPLOITING-RELATED METHODS
+  ##  EXPLOITING-RELATED METHODS ##
+
   # exploit_resources recieves input units of a given energetic resource
   # and stores them in input_buffer to progressively consume them
   # at a rate 1 unit per minute
   #
-  # Each cosumed item generates @p_base_rate exploited resource units
+  # Each cosumed item generates @production_base_rate exploited resource units
   #
   def exploit_resource(resource)
-    input_buffer = resource.value
-    output_buffer = 0
-    t = calculate_exploiting_time(resource)
-    t.times do
-      # Consume 1 energy resource unit 
-      input_buffer -= 1
-      # Each consuming step should last 1 minute but 
-      # for developing purposes sleep time is lowered to 0.1 minutes
-      # sleep 60 
-      #
-      resource.consume(1)
-      sleep 0.1
-      # Generate the regular resource unit at a @p_base_rate rate
-      # and store it at output_buffer
-      #
-      output_buffer += @current_p_rate
-      puts "Producing #{output_buffer} units"
+    # resource must be the same energetic type set to the facility
+    if resource.type == @res_energy_type
+      input_buffer = resource.value
+      output_buffer = 0
+      t = calculate_exploiting_time(resource)
+      t.times do
+        # Consume 1 energy resource unit
+        input_buffer -= 1
+        # Each consuming step should last 1 minute but
+        # for developing purposes sleep time is lowered to 0.1 minutes
+        # sleep 60
+        #
+        resource.consume(1)
+        sleep 0.1
+        # Generate the regular resource unit at a @production_base_rate rate
+        # and store it at output_buffer
+        #
+        output_buffer += @current_production_rate
+        puts "Producing #{output_buffer} units"
+      end
+      # Return what have been stored in ouput_buffer
+      Resource.new(@res_output, output_buffer)
     end
-    # Return what have been stored in ouput_buffer
-    Resource.new(@res_output, output_buffer)
   end
 
   # calculate_exploiting_time calculates how many
@@ -62,91 +66,98 @@ class Facility
   # 50 minutes of facility working uptim
   # 
   def calculate_exploiting_time(resource)
-    # Suppose 1 input energy-unit unit gives 1 energy-minute
     resource.value * resource.energy_rate
   end
 
-  ## UPGRADE-RELATED METHODS
-  def add_reserve(resource_investment)
-     @res_reserve += resource_investment
+  ## UPGRADE-RELATED METHODS ##
+
+  # Increase facility level and its production rate
+  def increase_level
+    @level += 1
+    set_production_current_level_rate
   end
 
+  # Set new production rate depending current facility level
+  def set_production_current_level_rate
+    @current_production_rate = @production_base_rate + calculate_production_rate_increment(@level)
+  end
+
+  # Upgrade facility level it's enough resources are invested
   def upgrade(resource)
     if enough?(resource)
       pay_upgrade_cost(resource)
-      add_reserve(upgrade_level_cost)
-      @level += 1
-      set_production_current_level_rate
+      increase_level
     end
   end
 
-  def pay_upgrade_cost(resource)
-    resource.value -= upgrade_level_cost
+  # Check if there's enough resources
+  def enough?(resource)
+    resource.type == @res_maint_type && resource.value >= upgrade_level_cost
   end
 
-  def enough?(resource)
-    resource.type == @res_maint && resource.value >= upgrade_level_cost
+  # Consume the resource invested and store that amount in @res_reserve resource
+  def pay_upgrade_cost(resource)
+    resource.consume(upgrade_level_cost)
+    @res_reserve.add(upgrade_level_cost)
   end
 
   def upgrade_level_cost
     Utilities.fibonacci(@level + 1) * 10
   end
 
-  def set_production_current_level_rate
-    @current_p_rate = @p_base_rate + calculate_production_rate_increment(@level)
-  end
-
   def calculate_production_rate_increment(level)
     Math.log2(level).round
   end
 
-  def downgrade(resource)
+  ## DOWNGRADE-RELATED METHODS ##
+
+  # Decrease facility level and its production rate
+  def decrease_level
+    @level -= 1
+    set_production_current_level_rate
+  end
+
+  # Downgrade facility level it's enough resources are invested
+  def downgrade
     if @level > 1
-      @level -= 1
-      r = return_upgrade_cost(resource)
-      set_production_current_level_rate
-      return r
+      decrease_level
+      return_upgrade_cost
     end
   end
 
-  def return_upgrade_cost(resource)
-    @res_reserve -= upgrade_level_cost
-    resource.value += upgrade_level_cost
+  # Get the resource stored in @res_reserve and return this amount back in a new resource
+  def return_upgrade_cost
+    @res_reserve.consume(upgrade_level_cost)
+    Resource.new(@res_reserve.type, upgrade_level_cost)
   end
 
-  def energy_rates(e_resource)
-    case e_resource
-    when :coal
-      1
-    end   
-  end
-
+  # Set production rate base levels when a facility is instantiated
   def set_production_base_rate 
-    case @res_output
+    case @res_output_type
     when :timber
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :iron
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :gold
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :copper
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :diamond
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :clay
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :marble
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :coal
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :oil
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :gas
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :water
-      @p_base_rate = 1
+      @production_base_rate = 1
     when :uranium
-      @p_base_rate = 1
+      @production_base_rate = 1
     end
   end
 end
